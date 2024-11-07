c2e92b3ccfa534a3d8e182d5b6cc92e3d02606d3
SR <stefano-ras@live.nl>
2024-11-07 11:56:09 +0100

    Les 7-7

--- JavaScript-main/07-document-object-model/opdracht7/js/script.js
+++ JavaScript-main/07-document-object-model/opdracht7/js/script.js
@@ -9,8 +9,30 @@ window.addEventListener('DOMContentLoaded', () => {
         document.querySelector(".content").appendChild(content);
     });
 
-    document.querySelector(".font").addEventListener("click", onChange);
-    function onChange() {
-        
-    }
+    document.querySelector(".font").addEventListener("change", () => {
+        let font = document.querySelector(".font").value;
+        document.querySelector(".content").style.fontFamily = font;
+    });
+
+    document.querySelector(".background").addEventListener("change", () => {
+        let background = document.querySelector(".background").value;
+        document.querySelector(".content").style.background = background;
+    });
+
+    document.querySelector(".color").addEventListener("change", () => {
+        let color = document.querySelector(".color").value;
+        document.querySelector(".content").style.color = color;
+    });
+
+    document.querySelector(".fontsize").addEventListener("input", () => {
+        let fontsize = document.querySelector(".fontsize").value;
+        document.querySelector(".content").style.fontSize = fontsize + "px";
+        console.log(fontsize);
+    });
+    
+    document.querySelector(".delete").addEventListener("click", () => {
+        const child = document.querySelector(".content > :last-child");
+        const parent = document.querySelector(".content");
+        parent.removeChild(child);
+    });
 });

ce1f040676c4c3d301d47185fdaa88451e1b3806
SR <stefano-ras@live.nl>
2024-11-14 09:17:45 +0100

    9-2

--- JavaScript-main/09-reguliere-expressies/opdracht2/js/script.js
+++ JavaScript-main/09-reguliere-expressies/opdracht2/js/script.js
@@ -1,11 +1,13 @@
 const phone = /^(\+31 6 |06)(-| |)(\d{8})/;
+const warning = document.querySelector(".warning");
 window.addEventListener('DOMContentLoaded', () => {
     document.querySelector("form").addEventListener("submit", (e) => {
         e.preventDefault();
         let input = document.querySelector(".phone").value;
-        console.log(input);
         if(!phone.test(input)) {
-            console.warn("Invalid phone number");
+            console.warn("Invalid phone number!");
+            warning.innerHTML = "Invalid phone number!";
+            warning.style.color = "red";
         }
     });
 });

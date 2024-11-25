407df7613be284ace18b2b4a2e9c2b83d7a08a42
SR <stefano-ras@live.nl>
2024-11-25 11:38:31 +0100

    8-2

--- JavaScript-main/08-browser-object-model/opdracht2/js/script.js
+++ JavaScript-main/08-browser-object-model/opdracht2/js/script.js
@@ -1,9 +1,15 @@
 document.querySelector("#open").addEventListener("click", () => {
     const open = window.open("", "_blank", "width=300,height=200");
     open.document.open();
-    open.document.write(new Date());
+    open.document.write(
+        '<html><head><title>Welke dag is het?</title><link rel="stylesheet" type="text/css" href="css/style.css"></head>'
+        + new Date() + '</body></html>'
+    );
+    open.document
     open.document.close();
     document.querySelector("#close").addEventListener("click", () => {
-        open.window.close();
+        if(confirm("Weet je het zeker>") == true) {
+            open.window.close();
+        }
     });
 });

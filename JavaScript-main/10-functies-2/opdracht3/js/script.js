21c8e7b7ba69c97e919bcc1fbc055061218d34b2
SR <stefano-ras@live.nl>
2024-12-17 11:27:30 +0100

    10-3

--- JavaScript-main/10-functies-2/opdracht3/js/script.js
+++ JavaScript-main/10-functies-2/opdracht3/js/script.js
@@ -3,26 +3,21 @@ const persons = [
 	{name: "Ben", age: 23, loc: "Alkmaar"},
 	{name: "Jan", age: 31, loc: "Haarlem"},
 	{name: "Pim", age: 77, loc: "Bergen"},
-	{name: "Tim", age: 54, loc: "Oudorp"},
+	{name: "Tim", age: 54, loc: "Oudorp"}
 ];
 
 const formatPersons = persons => {
     persons.name = persons.name.substring(0,1) + ".";
     persons.age = persons.age + 1;
     persons.loc = persons.loc.toUpperCase();
-    console.log(persons);
+    return persons;
 };
 
-const formattedpersons = new Array(persons.map(formatPersons));
+const formattedpersons = persons.map(formatPersons);
 
 console.log(formattedpersons);
 
 for(let i = 0; i < formattedpersons.length; i++) {
-    document.querySelector("body").innerHTML = "<section><h1>" + formattedpersons.name +
-    "</h1><p>" + formattedpersons.age + "</p><p>" + formattedpersons.loc + "</p></section>";
+    document.querySelector("body").innerHTML += "<section><h1>" + formattedpersons[i].name +
+    "</h1><p>" + formattedpersons[i].age + "</p><p>" + formattedpersons[i].loc + "</p></section>";
 }
-
-/* formattedpersons.forEach(person => {
-    document.querySelector("body").innerHTML = "<section><h1>" + person.name +
-    "</h1><p>" + person.age + "</p><p>" + person.loc + "</p></section>";
-}); */

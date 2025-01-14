93289549b62da426de9f42abd6dcbb9b34ad9336
SR <stefano-ras@live.nl>
2025-01-14 09:33:47 +0100

    9-7

--- JavaScript-main/09-reguliere-expressies/opdracht7/js/script.js
+++ JavaScript-main/09-reguliere-expressies/opdracht7/js/script.js
@@ -1,8 +1,11 @@
-let text = document.querySelector("p");
+const text = document.querySelector("p");
 const formula = text.innerHTML.match(/\b([A-Z]{2} \d{5})\b/g);
 
 let i = 0
+let content = '';
 while(i < formula.length) {
-    text.innerHTML = text.innerHTML + "<li>" + formula[i] + "</li>";
-    i++
+    content += `<li>${formula[i]}</li>`;
+    i++;
 }
+
+text.innerHTML += '<ul>' + content + '<ul>';
